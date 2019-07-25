8efbbf6b6a98fe9351eac38cabb5156a7bb287ec
Beka Latsabidze <blatsabidze@outlook.com>
2019-07-25 11:24:09 +0400

    First Commit

---
+++ Appocalypto/Mob.cs
@@ -0,0 +1,72 @@
+﻿using SAPbouiCOM;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appocalypto
+{
+    public class Mob
+    {
+        public void Run(int Minutes = 1)
+        {
+            var orgPID = getProcessID();
+
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(Minutes);
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                var res = getProcessID();
+
+                if (res != orgPID)
+                {
+                    System.Windows.Forms.Application.Exit();
+                }
+
+            }, null, startTimeSpan, periodTimeSpan);
+        }
+
+        private int getProcessID()
+        {
+            var gui = new SboGuiApi();
+            try
+            {
+                gui.Connect("0030002C0030002C00530041005000420044005F00440061007400650076002C0050004C006F006D0056004900490056");
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+
+            int sessionID = Process.GetCurrentProcess().SessionId;
+
+            var application = gui.GetApplication();
+
+            var appId = application.AppId;
+            var processes = Process.GetProcessesByName(@"SAP Business One");
+
+            foreach (var process in processes.Where(x => x.SessionId == sessionID))
+            {
+                try
+                {
+                    var processAppId = gui.GetAppIdFromProcessId(process.Id);
+                    if (appId == processAppId)
+                    {
+                        return process.Id;
+                    }
+                }
+                catch (COMException)
+                {
+                    // GetAppIdFromProcessId will throw when the current process is not the one we are connected to
+                    continue;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Appocalypto/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Appocalypto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Appocalypto")]
+[assembly: AssemblyCopyright("RSM ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1a379b58-8f53-4e71-a470-7b7dfec273e8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

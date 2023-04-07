1e85a15fa82ef8df56b16afc1ed6c3754d3ad140
Beka Latsabidze <blatsabidze@outlook.com>
2023-04-07 18:23:53 +0400

    Co-authored-by: blatsabidze@outlook.com Co-authored-by: tangarinescott@gmail.com

--- Appocalypto/Mob.cs
+++ Appocalypto/Mob.cs
@@ -1,11 +1,8 @@
 ﻿using SAPbouiCOM;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
 
 namespace Appocalypto
